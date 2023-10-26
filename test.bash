d546ada279baee4f9677c3d08d29f18138caad95
Ryohei Tsuchida <otsuchiprogram@gmail.com>
2023-10-26 12:49:54 +0900

    testの更新yamada.bashの削除

--- test.bash
+++ test.bash
@@ -9,9 +9,18 @@ ng () {
 
 res=0
 
-### I/O TEST ###
+### I/O ###
 out=$(seq 5 | ./plus)
-[ "${out}" = 15.0 ] || ng ${LINENO}
+[ "${out}" = 15 ] || ng ${LINENO}
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]        || ng ${LINENO}
+[ "${out}" = "" ]   || ng ${LINENO}
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]        || ng ${LINENO}
+[ "${out}" = "" ]   || ng ${LINENO}
 
 [ "${res}" = 0 ] && echo OK
 exit $res
